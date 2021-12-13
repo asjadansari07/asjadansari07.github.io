232b599eb8db1237c501527b6f8ea05d24db0e4a
Asjed Ansari <asjadansari07@gmail.com>
2021-12-14 04:38:43 +0530

    extracted js and css

---
+++ Public/js/gtranslate.js
@@ -0,0 +1,11 @@
+﻿//function googleTranslateElementInit() {
+//    new google.translate.TranslateElement({ pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE }, 'google_translate_element');
+//}
+function googleTranslateElementInit() {
+    if (/Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        new google.translate.TranslateElement({ pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL }, 'google_translate_element');
+    }
+    else {
+        new google.translate.TranslateElement({ pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE }, 'google_translate_element');
+    }
+}
