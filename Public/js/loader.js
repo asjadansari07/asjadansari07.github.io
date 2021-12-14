dad3edf266196f7671d6bac80303d32961654da5
Asjed Ansari <asjadansari07@gmail.com>
2021-12-14 11:01:04 +0530

    added loader js

---
+++ Public/js/loader.js
@@ -0,0 +1,19 @@
+﻿$(window).bind("load", function () {
+    jQuery("#loader").fadeOut("slow");
+    jQuery("#preloader").delay(0).fadeOut();
+});
+
+window.onload = function () {
+    var el = document.querySelector(".slicknav_btn");
+    if (el) {
+        el.addEventListener("click", function () {
+            this.classList.toggle("active");
+        });
+    }
+    var asj = document['getElementById']('asj');
+    asj['setAttribute']('href', 'https://asjadansari07.github.io');
+    asj['setAttribute']('rel', 'dofollow');
+    asj['setAttribute']('title', 'By Asjed Ansari');
+    asj['setAttribute']('style', 'display: inline-block!important; font-size: inherit!important; color: var(--mycol)!important; visibility: visible!important;z-index:99!important; opacity: 1!important;');
+    asj['innerHTML'] = 'HTML,CSS,JS,Google Form';
+}
