649e69403fc87870a3170a0a989b95334cbaf46a
Asjed Ansari <asjadansari07@gmail.com>
2021-12-16 01:11:31 +0530

    lazyload error resolve

--- Public/js/loader.js
+++ Public/js/loader.js
@@ -1,32 +1,32 @@
-﻿//document.addEventListener("DOMContentLoaded", function () {
-//    var lazyloadImages = document.querySelectorAll("img.lazyload");
-//    var lazyloadThrottleTimeout;
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var lazyloadImages = document.querySelectorAll("img.lazyload");
+    var lazyloadThrottleTimeout;
 
-//    function lazyload() {
-//        if (lazyloadThrottleTimeout) {
-//            clearTimeout(lazyloadThrottleTimeout);
-//        }
+    function lazyload() {
+        if (lazyloadThrottleTimeout) {
+            clearTimeout(lazyloadThrottleTimeout);
+        }
 
-//        lazyloadThrottleTimeout = setTimeout(function () {
-//            var scrollTop = window.pageYOffset;
-//            lazyloadImages.forEach(function (img) {
-//                if (img.offsetTop < (window.innerHeight + scrollTop)) {
-//                    img.src = img.dataset.src;
-//                    img.classList.remove('lazy');
-//                }
-//            });
-//            if (lazyloadImages.length == 0) {
-//                document.removeEventListener("scroll", lazyload);
-//                window.removeEventListener("resize", lazyload);
-//                window.removeEventListener("orientationChange", lazyload);
-//            }
-//        }, 20);
-//    }
+        lazyloadThrottleTimeout = setTimeout(function () {
+            var scrollTop = window.pageYOffset;
+            lazyloadImages.forEach(function (img) {
+                if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                    img.src = img.dataset.src;
+                    img.classList.remove('lazy');
+                }
+            });
+            if (lazyloadImages.length == 0) {
+                document.removeEventListener("scroll", lazyload);
+                window.removeEventListener("resize", lazyload);
+                window.removeEventListener("orientationChange", lazyload);
+            }
+        }, 20);
+    }
 
-//    document.addEventListener("scroll", lazyload);
-//    window.addEventListener("resize", lazyload);
-//    window.addEventListener("orientationChange", lazyload);
-//});
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+});
 
 ////<![CDATA[
 //$(function () {
