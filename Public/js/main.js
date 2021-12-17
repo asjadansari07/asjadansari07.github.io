19708687e0a582132e9cad67555a563d490da3c7
Asjed Ansari <asjadansari07@gmail.com>
2021-12-18 02:47:44 +0530

    Update main.js

--- Public/js/main.js
+++ Public/js/main.js
@@ -227,7 +227,7 @@ let isMobileDevice = regexp.test(details);
             } else { 
                 ele.css("font-size","7px"); 
                 ele.css("line-height","10px");
-                ele.css("width","60%");
+                ele.css("width","55%");
 
             }
             //ele.css("font-size","4px"); 
