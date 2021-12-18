46f7cce4f4d826132bcce7d68f22be61e7478430
Asjed Ansari <asjadansari07@gmail.com>
2021-12-19 04:40:56 +0530

    Update main.js

--- Public/js/main.js
+++ Public/js/main.js
@@ -134,7 +134,7 @@ if (isMatch ) {
 let details = navigator.userAgent; 
 //let regexp = /android|iphone|kindle|ipad/i; 
 let regexp = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|kindle|Opera Mini/i;
-if(details.contains('wv'))
+if(regexp.includes('wv'))
 {
     document.write('opened in webview');
 }
