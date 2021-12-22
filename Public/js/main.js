f3ab9b9c131049cf2c937c9ce7cad1ab80ac8d81
Asjed Ansari <asjadansari07@gmail.com>
2021-12-23 03:59:55 +0530

    updated form

--- Public/js/main.js
+++ Public/js/main.js
@@ -309,11 +309,11 @@ let isMobileDevice = regexp.test(details);
     function setDefaultColor(){
         document.documentElement.style.setProperty('--mycol', '#FF0000', 'important');
         document.documentElement.style.setProperty('--background', '#FF0000', 'important');
-        //if(w>1200 || (w<=1200 && w>1100) || (w<=1100 && w>980) || (w<=980 && w>880) || (w<=880 && w>768) || (w<=768 && w>767)){            
-        //    $('#googleForm').attr("src", "https://docs.google.com/forms/d/e/1FAIpQLScS8s6OrBD3UmDvtTZqPg3j1WcmWptpsubVhLoSqgyY51fnng/viewform?embedded=true").css("filter","invert(0.9)");
-        //}else{            
-        //    $('#googleForm').attr("src", "https://docs.google.com/forms/d/e/1FAIpQLSex6H7IrFV1OfAGRVnfflw9dv2Z0F2vCTQ2NcoWmL7Q4WPyOg/viewform?embedded=true");
-        //}
+        if(w>1200 || (w<=1200 && w>1100) || (w<=1100 && w>980) || (w<=980 && w>880) || (w<=880 && w>768) || (w<=768 && w>767)){            
+            $('#googleForm').attr("data-src", "https://docs.google.com/forms/d/e/1FAIpQLScS8s6OrBD3UmDvtTZqPg3j1WcmWptpsubVhLoSqgyY51fnng/viewform?embedded=true").css("filter","invert(0.9)");
+        }else{            
+            $('#googleForm').attr("data-src", "https://docs.google.com/forms/d/e/1FAIpQLSex6H7IrFV1OfAGRVnfflw9dv2Z0F2vCTQ2NcoWmL7Q4WPyOg/viewform?embedded=true");
+        }
     }    
                 
 var counter = 0;
