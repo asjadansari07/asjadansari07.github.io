427b4ef20cf2f11ff9c1b54700f1b68806fb5747
Asjed Ansari <asjadansari07@gmail.com>
2021-12-27 23:46:50 +0530

    Update main.js

--- Public/js/main.js
+++ Public/js/main.js
@@ -102,9 +102,9 @@ disableoptions();
 //if website steal-start
 (function () { 
     //const rgx = new RegExp(/(domainName)|(:portNumUsedInLocalDev)|/g); 
-    const rgx = new RegExp('localhost:2435');
+    //const rgx = new RegExp('localhost:2435');
     //const rgx = new RegExp('localhost:8426');
-    //const rgx = new RegExp('asjadansari07.github.io');
+    const rgx = new RegExp('asjadansari07.github.io');
 const host = window.location.host; 
 const isMatch = !host.match(rgx); 
  
