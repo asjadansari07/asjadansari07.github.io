acbd2d0b6ccc0fee1097f88978453d9319cacb77
Asjed Ansari <asjadansari07@gmail.com>
2021-12-27 07:28:32 +0530

    Update main.js

--- Public/js/main.js
+++ Public/js/main.js
@@ -567,11 +567,14 @@ function changeForm(w,col){
         else{
 
             if($("body").css("background-color").replace(/\s/g,'')=="rgb(0,0,0)"){  
-                $('#googleForm').css("filter","none");                
+                $('#googleForm').css("filter","none");
+                $('#googleForm').attr("data-src","");
+                $('#googleForm').attr("data-src",gform);
                 
             }else{
                 $('#googleForm').css("filter","invert(0.9)");               
-                
+                $('#googleForm').attr("data-src","");
+                $('#googleForm').attr("data-src",gform);
             }        
         }
     }
