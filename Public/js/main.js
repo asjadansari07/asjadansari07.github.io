1ed0a95d952cab2e1095de24cbecc0ea2d1e77bc
Asjed Ansari <asjadansari07@gmail.com>
2022-01-09 05:34:12 +0530

    Update main.js

--- Public/js/main.js
+++ Public/js/main.js
@@ -107,8 +107,8 @@ disableoptions();
 (function () { 
     //const rgx = new RegExp(/(domainName)|(:portNumUsedInLocalDev)|/g); 
     //const rgx = new RegExp('localhost:2435');
-    const rgx = new RegExp('localhost:8426');
-    //const rgx = new RegExp('asjadansari07.github.io');
+    //const rgx = new RegExp('localhost:8426');
+    const rgx = new RegExp('asjadansari07.github.io');
 const host = window.location.host; 
 const isMatch = !host.match(rgx); 
  
