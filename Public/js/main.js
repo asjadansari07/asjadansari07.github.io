80ffca566ceb710a539cf297bda6269b8d2ac1c8
Asjed Ansari <asjadansari07@gmail.com>
2022-01-13 00:56:46 +0530

    Update main.js

--- Public/js/main.js
+++ Public/js/main.js
@@ -344,8 +344,8 @@ $(document).ready(function () {
 
             
             $('#google_translate_element').css('display','none');
-            $('#ie').css('display','inline-block');
-            $('#ieform').css('display','inline-block').css('color','#ff0000');
+            $('#iegtranslate').css('display','inline-block');
+            $('#iegform').css('display','inline-block').css('color','#ff0000');
             
             
         }
