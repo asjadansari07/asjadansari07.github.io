a526d6b34c8b522e07b869832d2d512cc2baff72
Asjed Ansari <asjadansari07@gmail.com>
2022-01-21 03:16:17 +0530

    Update main.js

--- Public/js/main.js
+++ Public/js/main.js
@@ -46,7 +46,7 @@ $(document).ready(function () {
     let regexp = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|kindle|Opera Mini/i;
     let isMobileDevice = regexp.test(details); 
     //let isWebview=details.toString().includes('wv');
-    let isWebview=function(){if((details.toString().indexOf('wv'))!==-1)return true};
+    let isWebview=function(){if((details.toString().indexOf('wv'))!==-1){return true}else{return false}};
     //check browser in mobile-end
 
 
