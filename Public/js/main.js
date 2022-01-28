6fc503422b2d229ca6c3c34bc9b8d98aea31cc9e
Asjed Ansari <asjadansari07@gmail.com>
2022-01-29 05:11:01 +0530

    gtranslate hide onload

--- Public/js/main.js
+++ Public/js/main.js
@@ -105,6 +105,7 @@ $(document).ready(function () {
                         $(".main-gallery").css("background","url(/Public/images/head-back1.webp) no-repeat fixed 50% 50%/cover");
                         $(".insta-wrap").css("background","url(/Public/images/map.webp) no-repeat fixed 50% 50%/cover").css("position","relative");
                         $(".insta-wrap .filter").css("display","block");
+                        $("#google_translate_element").css("display","block");
                     }
                 }
             }
