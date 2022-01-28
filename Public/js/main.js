5609a41b74192ce9010f0523f3bda8f4d0e4962f
Asjed Ansari <asjadansari07@gmail.com>
2022-01-29 04:07:49 +0530

    css img lazy

--- Public/js/main.js
+++ Public/js/main.js
@@ -31,6 +31,7 @@ $(document).ready(function () {
                 $(".counter-box").css("background","url(/Public/images/sg-back.webp) no-repeat");
                 $(".main-gallery").css("background","url(/Public/images/head-back1.webp) no-repeat fixed 50% 50%/cover");
                 $(".insta-wrap").css("background","url(/Public/images/map.webp) no-repeat fixed 50% 50%/cover").css("position","relative");
+                $(".insta-wrap .filter").css("display","block");
             }
 
             else {
