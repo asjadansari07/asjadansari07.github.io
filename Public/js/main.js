a5b47e8edd35d877bb352e8f76dcd5299fcbe71a
Asjed Ansari <asjadansari07@gmail.com>
2022-01-29 15:56:26 +0530

    updated

--- Public/js/main.js
+++ Public/js/main.js
@@ -93,9 +93,9 @@ $(document).ready(function () {
                         //user is at the top of the page; no need to show the back to top button
                         $(".counter-box").css("background","url(/Public/images/sg-back.webp) no-repeat fixed 50% 50%/cover");
                         $(".main-gallery").css("background","url(/Public/images/head-back1.webp) no-repeat fixed 50% 50%/cover");
-                        $(".insta-wrap").css("background","url(/Public/images/map.webp) no-repeat fixed 50% 50%/cover").css("position","relative");
-                        $(".insta-wrap .filter").css("display","block");
-                        $("#google_translate_element").css("display","block");
+                        $(".insta-wrap").css("background","url(/Public/images/map.webp) no-repeat fixed 50% 50%/cover").css("position","relative");                        
+                        $(".insta-wrap .filter,#google_translate_element,#Lqr,#Sqr").css("display","block");
+                        
                         //$("#googleForm").css("display","block");
 
                         //$.getScript("Public/js/gtranslate.js", function(){
