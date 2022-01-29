a3b875e1487969552cb695227ee1c25d9f4e3438
Asjed Ansari <asjadansari07@gmail.com>
2022-01-29 17:57:53 +0530

    Update main.js

--- Public/js/main.js
+++ Public/js/main.js
@@ -125,8 +125,9 @@ $(document).ready(function () {
 
                         $('#zoom').load('Public/images/sqr.svg');                           
                         $('#pop-up').load('Public/images/lqr.svg'); 
-                        $('image[width="73"]').attr('xlink:href','');
-                        $('image[width="73"]').attr('xlink:href','/Public/images/asj_2.png');
+                        //$('image[width="73"]').attr('xlink:href','');
+                        //$('image[width="73"]').attr('xlink:href','/Public/images/asj_2.png');
+                        $('#pop-up').replace('webp','png');
 
                         //document.getElementById("zoom").innerHTML='<object type="text/html" data="Public/images/sqr.svg" ></object>';
                         //document.getElementById("pop-up").innerHTML='<object type="text/html" data="Public/images/lqr.svg" ></object>';
@@ -135,8 +136,9 @@ $(document).ready(function () {
                 else{
                     $('#zoom').load('Public/images/sqr.svg');                           
                     $('#pop-up').load('Public/images/lqr.svg'); 
-                    $('image[width="73"]').attr('xlink:href','');
-                    $('image[width="73"]').attr('xlink:href','/Public/images/asj_2.png');
+                    //$('image[width="73"]').attr('xlink:href','');
+                    //$('image[width="73"]').attr('xlink:href','/Public/images/asj_2.png');
+                    $('#pop-up').replace('webp','png');
                 }
             }
         };
