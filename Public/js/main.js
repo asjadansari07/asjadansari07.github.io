26cec3a9e9859b52b15872b675d07d02ae12c3d2
Asjed Ansari <asjadansari07@gmail.com>
2022-02-05 04:50:56 +0530

    js

--- Public/js/main.js
+++ Public/js/main.js
@@ -96,8 +96,8 @@ $(document).ready(function() {
                         $(".main-gallery").css("background", "url(/Public/images/head-back1.webp) no-repeat fixed 50% 50%/cover");
                         $(".insta-wrap").css("background", "url(/Public/images/map.webp) no-repeat fixed 50% 50%/cover").css("position", "relative");
                         $(".insta-wrap .filter,#Lqr,#Sqr").css("display", "block");
-                        initformlang();
-                        isinitformlang=true;
+                        if(!isinitformlang)
+                        {initformlang();}                   
                         //$("#google_translate_element").html($("#google_translate_element").html().replace("Offline Translation Is'nt Avaliable",""));
                         //let myScript1 = document.createElement("script");
                         //myScript1.setAttribute("src", "Public/js/gtranslate.js");
@@ -143,6 +143,7 @@ $(document).ready(function() {
         iframe1.attr("src","");
         iframe1.attr("src", "https://docs.google.com/forms/d/e/1FAIpQLScMJ6lFRbePTtDsSnTRCXPQEOhP8K8nVK5Xb8p-qT69QssbpQ/viewform?embedded=true");
         iframe1.css("display", "block");
+        isinitformlang=true;
     }
     window.addEventListener("scroll", function() {
         cssImgLazyLoad();
