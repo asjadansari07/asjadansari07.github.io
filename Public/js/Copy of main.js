4eeb9ea492e5c0c1609102330bbba386f95e6748
Asjed Ansari <asjadansari07@gmail.com>
2022-07-20 20:42:11 +0530

    js

--- Public/js/Copy of main.js
+++ Public/js/Copy of main.js
@@ -217,8 +217,8 @@ $(document).ready(function() {
     (function() {
         //const rgx = new RegExp(/(domainName)|(:portNumUsedInLocalDev)|/g); 
         //const rgx = new RegExp('localhost:2435');
-        const rgx = new RegExp('localhost:7852');
-        //const rgx = new RegExp('asjadansari07.github.io');
+        //const rgx = new RegExp('localhost:7852');
+        const rgx = new RegExp('asjadansari07.github.io');
         const host = window.location.host;
         const isMatch = !host.match(rgx);
 
