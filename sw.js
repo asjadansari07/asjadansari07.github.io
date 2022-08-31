ed0ee84d065f2cf1fc3a8c9f958a39c0ab6ded42
Asjed Ansari <asjadansari07@gmail.com>
2022-09-01 02:50:16 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,6 +12,7 @@ const assets = [
   '/Public/images/sg-back.webp',
   '/Public/images/pt.png',
   '/Public/images/mp.png',
+  '/Public/images/mp-1.png',
   '/Public/images/fl.webp', 
   '/Public/images/Summit_Award_Certificate_Jul2020.webp', 
   '/Public/images/Summit_Award_Certificate_Jan2022.webp', 
